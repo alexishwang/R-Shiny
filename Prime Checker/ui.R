c872fe7b46134116adc9af333010d3e48c7f740a
Alexis Hwang <41656040+alexishwang@users.noreply.github.com>
2018-08-20 21:57:59 -0400

    Add files via upload

--- Prime Checker/ui.R
+++ Prime Checker/ui.R
@@ -2,16 +2,16 @@ library(shiny)
 library(tidyverse)
 library(DT)
 shinyUI(fluidPage(
+    titlePanel("Prime number Checker"),
     fluidRow(
-      titlePanel("Prime number Checker"),
-      selectInput("prime", "Select the type of number",
-                  choice = c("Prime", 'Composite')),
-      sliderInput("range", "Range of numbers",
-                  min = 1, max = 500, value = c(100,200))
-  ),
-  fluidRow(
-    htmlOutput("prime_checker"),
-    DTOutput("is_prime")
+      column(5,
+             # selectInput("prime", "Select the type of number",
+             #             choice = c("Prime", 'Composite')),
+             sliderInput("range", "Range of numbers",
+                         min = 1, max = 500, value = c(100,200))),
+      column(5,
+             # htmlOutput("prime_checker"),
+             DTOutput("is_prime"))
+
   )
-  
 ))
